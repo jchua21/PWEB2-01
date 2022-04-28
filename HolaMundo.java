e79a7b62e3983c2c359717e4d5dbfe1faac7aba0
JChua21 <jchua@unsa.edu.pe>
2022-04-28 17:17:54 -0500

    Se agrego nombre de Alumno

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo{
 	public static void main (String [] args){
             System.out.println("Hola Mundo");
 	    System.out.println("Bienvenidos a PWEB2");
-	}
+	    System.out.println("Atte : Jean Carlo Chua Aguilar");
+	};
 }
